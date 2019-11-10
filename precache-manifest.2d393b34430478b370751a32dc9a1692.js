4e8567e93b791a4f8bbd85d0e9cc853676de733b
ufoo68 <mitarou.yuutarou@gmail.com>
2019-11-10 20:47:07 +0900

    Updates

--- precache-manifest.2d393b34430478b370751a32dc9a1692.js
+++ precache-manifest.2d393b34430478b370751a32dc9a1692.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "225785f2259ff1f1e2c7f568ed376636",
+    "revision": "dc1ce0afa685297737917608a4823802",
     "url": "/react-signal-light/index.html"
   },
   {
-    "revision": "e7c4166fc844696fc0ac",
+    "revision": "5134582db25bc7c6561a",
     "url": "/react-signal-light/static/css/main.e789b65c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-signal-light/static/js/2.f89a5cab.chunk.js"
   },
   {
-    "revision": "e7c4166fc844696fc0ac",
-    "url": "/react-signal-light/static/js/main.ce1ed2c0.chunk.js"
+    "revision": "5134582db25bc7c6561a",
+    "url": "/react-signal-light/static/js/main.818767a4.chunk.js"
   },
   {
     "revision": "c700b2008711f31350a6",
