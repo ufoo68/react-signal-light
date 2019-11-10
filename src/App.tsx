0a7f4c8cde97d5c53ac1352c2ff4e1909c52de82
ufoo68 <mitarou.yuutarou@gmail.com>
2019-11-10 16:34:36 +0900

    react implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from 'react';
+import { Buffer } from 'buffer'
+import './App.scss';
+import imgOn from './assets/on.png'
+import imgOff from './assets/off.png'
+import { BluetoothDevice, BluetoothRemoteGATTService } from 'liff-type';
 
-const App: React.FC = () => {
+const liff = window.liff
+
+const App: FC = () => {
+const [lightValue, setLightValue] = useState<string>('0')
+const [lightOn, setLightOn] = useState<boolean>(false)
+
+  const sendValue = () => {
+    liff.init({ liffId: process.env.REACT_APP_LIFF_ID as string }).then(() => {
+      liff.initPlugins(['bluetooth']).then(() => 
+        liffCheckAvailablityAndDo(() => liffRequestDevice())
+      )
+    })
+  }
+  
+  const liffCheckAvailablityAndDo = (callbackIfAvailable: () => void) => {
+    liff.bluetooth.getAvailability().then(isAvailable => {
+        if (isAvailable) {
+            callbackIfAvailable()
+        } else {
+            setTimeout(() => liffCheckAvailablityAndDo(callbackIfAvailable), 10000)
+        }
+    })
+  }
+  
+  const liffRequestDevice = () => {
+    liff.bluetooth.requestDevice().then(device => {
+        liffConnectToDevice(device)
+    })
+  }
+  
+  const liffConnectToDevice = (device: BluetoothDevice) => {
+    device.gatt!.connect().then(() => {
+        device.gatt!.getPrimaryService(process.env.REACT_APP_USER_SERVICE_UUID as string).then(service => {
+            liffGetUserService(service);
+        })
+    })
+  }
+  
+  const liffGetUserService = (service: BluetoothRemoteGATTService) => {
+    const CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
+    service.getCharacteristic(CHARACTERISTIC_UUID).then(characteristic => {
+        characteristic.writeValue(Buffer.from(lightValue))
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="light">
+       <img src={lightOn ? imgOn : imgOff} alt="signal-light"/>
+      </div>
+      <button onClick={() => setLightOn(lightOn ? false : true)} className="button">
+        {lightOn ? 'OFF' : 'ON'}
+      </button>
     </div>
   );
 }
